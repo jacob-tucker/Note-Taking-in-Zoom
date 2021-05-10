821d2b5cd36e6df2d160c349a001adb64e929f8c
Brandonl555 <54605404+Brandonl555@users.noreply.github.com>
2021-05-10 18:41:03 -0500

    Finished duration slices tuples

--- postProcessing/postProcessNotes.py
+++ postProcessing/postProcessNotes.py
@@ -28,9 +28,10 @@ class NotesPostProcessor:
                         # Arrived at a timestamp, grab it, and store it in seconds
                         temp = self.getTimeStamp(stripped_line)
                         ts = self.convertToSeconds(temp)
-                        timeStamps.append(ts)
-                        print(ts)
-                #print(stripped_line)
+                        self.timeStamps.append(ts)
+            self.computeDurations()
+            print("TimeStamps: " + str(self.timeStamps))
+            print("Index, start, end, durations: " + str(self.durationSlices))
 
     def getTimeStamp(self, s):
         title, timeStamp = s.split(':', 1)
@@ -38,28 +39,38 @@ class NotesPostProcessor:
 
     def convertToSeconds(self, ts):
         ftr = [3600,60,1]
-        if ts.find("PM") != -1:
+        if ts.find("PM") == -1:
             ### time is in the morning, convert to seconds normally
-            ts.replace("PM", "")
-            ts.strip()
-            newTS = '0' + ts
-            print(newTS)
-            return sum([a*b for a,b in zip(ftr, map(int, newTS.split(':')))])
+            temp = ts.replace("AM", "")
+            return sum([a*b for a,b in zip(ftr, map(int, temp.split(':')))])
         else:
             ### time is in afternoon, must add 12 to hour and convert
-            ts.replace("AM", "")
-            ts.strip()
-            hours, min, sec = ts.split(':')
+            temp = ts.replace("PM", "")
+            hours, min, sec = temp.split(':')
             hours = str(int(hours) + 12)
-            afterNoonTS = '0' + hours + ':' + min + ':' + sec
+            afterNoonTS = hours + ':' + min + ':' + sec
             return sum([a*b for a,b in zip(ftr, map(int, afterNoonTS.split(':')))])
 
+    def checkEven(self):
+        if len(self.timeStamps) % 2 == 0:
+            return True
+        return False
 
-
+    def computeDurations(self):
+        if self.checkEven():
+            sliceIndex = 1
+            for i in range(1, len(self.timeStamps), 2):
+                curr_start = self.timeStamps[i-1]
+                curr_end = self.timeStamps[i]
+                curr_duration = curr_end - curr_start
+                self.durationSlices.append([sliceIndex, curr_start, curr_end, curr_duration])
+                sliceIndex += 1
+        else:
+            #print("There is not an even number of timestamps on the document, current length:" + str(len(self.timeStamps)))
+            return -1
 
 ###
 ### Testing
 ###
-
 p = NotesPostProcessor("test.txt")
 p.parseText()
