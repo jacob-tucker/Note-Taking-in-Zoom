01718589aa8fb84fd8a5dd9ba016937ab74e0cab
jacob-tucker <jacobtucker818@gmail.com>
2021-05-17 21:55:22 -0500

    no longer need any passed in values for python script

--- postProcessing/main.py
+++ postProcessing/main.py
@@ -57,12 +57,12 @@ def doClipping(start_time_seconds, name_of_folder):
 	# this gets the text from the google doc in srs.argv[1] and writes it
 	# to test.txt
 	# sys.argv[1] is the title the google doc you are looking for
-	for file_list in drive.ListFile({'q': "title contains '%s'" % str(sys.argv[1])}):
-		print('Received %s files from Files.list()' % len(file_list))
-		for eachFile in file_list:
-			print('title: %s, id: %s' % (eachFile['title'], eachFile['id']))
-			file = drive.CreateFile({'id': eachFile['id']})
-			file.GetContentFile('test.txt')
+	file_list =  drive.ListFile({'orderBy': "lastViewedByMeDate desc", "maxResults": 1}).GetList()
+	docFile = file_list[0]
+	print('Received %s files from Files.list()' % len(file_list))
+	print('title: %s, id: %s' % (docFile['title'], docFile['id']))
+	file = drive.CreateFile({'id': docFile['id']})
+	file.GetContentFile('test.txt')
 
 	# get all timestamp information
 	p = postProcessNotes.NotesPostProcessor("test.txt")
